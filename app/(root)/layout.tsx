94ccf65dcc9425e70e55175c8b2257ecac3272db
Jon Friend <JRF.VRM@gmail.com>
2025-03-13 16:24:39 -0500

    header, footer and theme toggler added

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,28 @@
+import Header from '@/components/shared/header';
+import Footer from '@/components/footer';
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <div className='flex h-screen flex-col'>
+//       <main className='flex-1 wrapper'>{children}</main>
+//     </div>
+//   );
+// }
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className='flex h-screen flex-col'>
+      <Header />
+      <main className='flex-1 wrapper'>{children}</main>
+      <Footer/>
+    </div>
+  );
+}
