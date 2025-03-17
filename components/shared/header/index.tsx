28b4d51a27a4031f535e8b8654b06166088b9193
Jon Friend <JRF.VRM@gmail.com>
2025-03-17 12:32:28 -0500

    responsive header added

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,10 +1,10 @@
-import { ShoppingCart, UserIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import ModeToggle from './mode-toggle';
-
-import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/constants';
+import Menu from './menu';
+// import { ShoppingCart, UserIcon } from 'lucide-react';
+// import ModeToggle from './mode-toggle';
+// import { Button } from '@/components/ui/button';
 
 const Header = () => {
   return (
@@ -24,21 +24,25 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className='space-x-2'>
-        <ModeToggle />
-          <Button asChild variant='ghost'>
-            <Link href='/cart'>
-              <ShoppingCart />
-              Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href='/sign-in'>
-              <UserIcon />
-              Sign In
-            </Link>
-          </Button>
-        </div>
+        
+        {/* <div className='space-x-2'>
+          <ModeToggle />
+            <Button asChild variant='ghost'>
+              <Link href='/cart'>
+                <ShoppingCart />
+                Cart
+              </Link>
+            </Button>
+            <Button asChild>
+              <Link href='/sign-in'>
+                <UserIcon />
+                Sign In
+              </Link>
+            </Button>
+        </div> */}
+
+        <Menu />
+
       </div>
     </header>
   );
