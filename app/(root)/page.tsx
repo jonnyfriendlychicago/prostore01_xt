b0549e5ba4c216079526f022559055435291b5fc
Jon Friend <JRF.VRM@gmail.com>
2025-03-17 11:17:18 -0500

    loading and not found pages added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,19 @@
 import { Button } from '@/components/ui/button';
 
+// // BEGIN: testing loading page; commOn/Off as needed
+// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// const Homepage = async () => {
+//   await delay(5000);
+//   return ( 
+//     <>  
+//       <h1>Hockey Playdate</h1>
+//       <Button>clickster</Button>
+//       </>
+// );
+// };
+// // END: testing loading page; 
+
 const Homepage = () => {
   return ( 
     <>  
