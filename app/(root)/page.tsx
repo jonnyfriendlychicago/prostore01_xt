a067e0423113eef1d5302ef1183848aef1d2d090
Jon Friend <JRF.VRM@gmail.com>
2025-03-18 09:57:10 -0500

    product display on homepage plus all filesComponents to make that happen

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
-import { Button } from '@/components/ui/button';
+// import { Button } from '@/components/ui/button';
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
 
 // // BEGIN: testing loading page; commOn/Off as needed
 // const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -14,13 +16,29 @@ import { Button } from '@/components/ui/button';
 // };
 // // END: testing loading page; 
 
-const Homepage = () => {
-  return ( 
-    <>  
-      <h1>Hockey Playdate</h1>
-      <Button>clickster</Button>
-      </>
-);
-}
+// const Homepage = () => {
+//   return ( 
+//     <>  
+//       <h1>Hockey Playdate</h1>
+//       <Button>clickster</Button>
+//       </>
+// );
+// }
+
+const HomePage = () => {
+  console.log(sampleData)
+  return (
+    <div className='space-y-8'>
+      <h2 className='h2-bold'>Latest Products</h2>
+      {/* <Button>clickster</Button> */}
+      {/* <ProductList title='Newest Arrivals' data={sampleData.products} /> */}
+      <ProductList 
+      title='Newest Arrivals' 
+      data={sampleData.products}
+        limit={4}
+       />
+    </div>
+  );
+};
+export default HomePage;
  
-export default Homepage;
